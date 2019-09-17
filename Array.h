4c8ee042eb36dd8913369b4e6b5634030bd140b0
Matt Norman <normanmr@ornl.gov>
2019-09-17 16:39:57 -0400

    changing name from copyTo[Host|Device] to deep_copy

--- Array.h
+++ Array.h
@@ -525,7 +525,7 @@ template <class T, int myMem> class Array {
   }
 
 
-  inline void copyToHost(Array<T,memHost> lhs) {
+  inline void deep_copy(Array<T,memHost> lhs) {
     if (myMem == memHost) {
       for (int i=0; i<totElems; i++) {
         lhs.myData[i] = myData[i];
@@ -542,7 +542,7 @@ template <class T, int myMem> class Array {
   }
 
 
-  inline void copyToDevice(Array<T,memDevice> lhs) {
+  inline void deep_copy(Array<T,memDevice> lhs) {
     if (myMem == memHost) {
       #ifdef __USE_CUDA__
         cudaMemcpy(lhs.myData,myData,totElems*sizeof(T),cudaMemcpyHostToDevice);
