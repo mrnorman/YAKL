da105894e6325d9d090c2a7cfb4334e26ea15aab
Matt Norman <normanmr@ornl.gov>
2020-11-09 09:07:19 -0500

    Fixing gfortran bug in gator_mod.F90 that didn't show up before

---
+++ unit/build/summit_debug.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+source $MODULESHOME/init/bash
+module purge
+module load DefApps gcc/8.1.1 cmake
+
+./cmakeclean.sh
+
+export CXXFLAGS="-O0 -g -DYAKL_DEBUG"
+export FFLAGS="-O0 -g"
+
+cmake ..
+
