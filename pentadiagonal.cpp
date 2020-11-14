c39fa217e1183a5f59b2198935249784e131b86c
Matt Norman <normanmr@ornl.gov>
2020-11-13 21:08:19 -0500

    pentadiagonal is working. Now to add cyclic pentadiagonal

--- pentadiagonal.cpp
+++ pentadiagonal.cpp
@@ -8,46 +8,44 @@ template <unsigned int n>
 void matrix_inverse(SArray<real,2,n,n> &a);
 
 
+template <unsigned int n>
+void pentadiagonal(SArray<real,1,n> const &a,
+                   SArray<real,1,n> const &b,
+                   SArray<real,1,n> const &c,
+                   SArray<real,1,n> const &d,
+                   SArray<real,1,n> const &e,
+                   SArray<real,1,n> const &f,
+                   SArray<real,1,n> &u);
+
+
 int main() {
-  unsigned int constexpr n = 3;
-  SArray<real,2,n,n> a;
-  SArray<real,2,n,n> ainv;
+  unsigned int constexpr n = 7;
+  SArray<real,1,n> a;
+  SArray<real,1,n> b;
+  SArray<real,1,n> c;
+  SArray<real,1,n> d;
+  SArray<real,1,n> e;
+  SArray<real,1,n> x;
+  SArray<real,1,n> y;
 
-  a(0,0) = 9;
-  a(1,0) = 2;
-  a(2,0) = 3;
-  a(0,1) = 4;
-  a(1,1) = 5;
-  a(2,1) = 6;
-  a(0,2) = 7;
-  a(1,2) = 8;
-  a(2,2) = 9;
+  a(0) = 0.0; a(1) = 0.0; a(2) = 0.2; a(3) = 0.9; a(4) = 4.0; a(5) = 2.2; a(6) = 1.1;
+  b(0) = 0.0; b(1) = 3.6; b(2) = 2.8; b(3) = 3.1; b(4) = 6.7; b(5) = 1.2; b(6) = 0.1;
+  c(0) = 4.1; c(1) = 2.2; c(2) = 6.2; c(3) = 8.5; c(4) = 3.8; c(5) = 3.7; c(6) = 2.1;
+  d(0) = 0.4; d(1) = 1.0; d(2) = 5.0; d(3) = 4.9; d(4) = 2.3; d(5) = 5.1; d(6) = 0.0;
+  e(0) = 0.5; e(1) = 6.1; e(2) = 2.9; e(3) = 4.5; e(4) = 0.7; e(5) = 0.0; e(6) = 0.0;
 
-  for (int i=0; i < n; i++) {
-    for (int j=0; j < n; j++) {
-      std::cout << std::setw(16) << a(j,i) << "  ";
-    }
-    std::cout << std::endl;
-  }
-  std::cout << "\n\n";
+  y(0) = 6.4;
+  y(1) = 35.4;
+  y(2) = 58.9;
+  y(3) = 96.6;
+  y(4) = 76.5;
+  y(5) = 72.7;
+  y(6) = 20.8;
 
-  matrix_inverse(a);
-
-  for (int i=0; i < n; i++) {
-    for (int j=0; j < n; j++) {
-      std::cout << std::setw(16) << a(j,i) << "  ";
-    }
-    std::cout << std::endl;
-  }
-  std::cout << "\n\n";
-
-  matrix_inverse(a);
+  pentadiagonal(a,b,c,d,e,y,x);
 
   for (int i=0; i < n; i++) {
-    for (int j=0; j < n; j++) {
-      std::cout << std::setw(16) << a(j,i) << "  ";
-    }
-    std::cout << std::endl;
+    std::cout << x(i) << "\n";
   }
 
 }
@@ -60,47 +58,44 @@ void pentadiagonal(SArray<real,1,n> const &a,
                    SArray<real,1,n> const &d,
                    SArray<real,1,n> const &e,
                    SArray<real,1,n> const &f,
-                   SArray<real,1,n> const &u) {
-// solves for a vector u of length n in the pentadiagonal linear system
-//  a_i u_(i-2) + b_i u_(i-1) + c_i u_i + d_i u_(i+1) + e_i u_(i+2) = f_i
-// input are the a, b, c, d, e, and f and they are not modified
+                   SArray<real,1,n> &u) {
+  // solves for a vector u of length n in the pentadiagonal linear system
+  //  a_i u_(i-2) + b_i u_(i-1) + c_i u_i + d_i u_(i+1) + e_i u_(i+2) = f_i
+  // input are the a, b, c, d, e, and f and they are not modified
 
-// in its clearest incarnation, this algorithm uses three storage arrays
-// called p, q and r. here, the solution vector u is used for r, cutting
-// the extra storage down to two arrays.
+  // in its clearest incarnation, this algorithm uses three storage arrays
+  // called p, q and r. here, the solution vector u is used for r, cutting
+  // the extra storage down to two arrays.
   SArray<real,1,n> p, q;
   real bet, den;
 
   // initialize elimination and backsubstitution arrays
-  if (c(1) .eq. 0.0)  stop 'eliminate u2 trivially'
-  bet  = 1.0d0/c(1)
-  p(1) = -d(1) * bet
-  q(1) = -e(1) * bet
-  u(1) =  f(1) * bet
+  bet  = static_cast<real>(1)/c(0);
+  p(0) = -d(0) * bet;
+  q(0) = -e(0) * bet;
+  u(0) =  f(0) * bet;
 
-  bet = c(2) + b(2)*p(1)
-  if (bet .eq. 0.0) stop 'bet singular in pentadiagonal'
-  bet  = -1.0d0/bet
-  p(2) = (d(2) + b(2)*q(1)) * bet
-  q(2) = e(2) * bet
-  u(2) = (b(2)*u(1) - f(2)) * bet
+  bet = c(1) + b(1)*p(0);
+  bet  = -static_cast<real>(1)/bet;
+  p(1) = (d(1) + b(1)*q(0)) * bet;
+  q(1) = e(1) * bet;
+  u(1) = (b(1)*u(0) - f(1)) * bet;
 
   // reduce to upper triangular
-  do i=3,n
-   bet = b(i) + a(i) * p(i-2)
-   den = c(i) + a(i)*q(i-2) + bet*p(i-1)
-   if (den .eq. 0.0) stop 'den singular in pentadiagonal'
-   den = -1.0d0/den
-   p(i) = (d(i) + bet*q(i-1)) * den
-   q(i) = e(i) * den
-   u(i) = (a(i)*u(i-2) + bet*u(i-1) - f(i)) * den
-  enddo
+  for (int i=2; i < n; i++) {
+    bet = b(i) + a(i) * p(i-2);
+    den = c(i) + a(i)*q(i-2) + bet*p(i-1);
+    den = -static_cast<real>(1)/den;
+    p(i) = (d(i) + bet*q(i-1)) * den;
+    q(i) = e(i) * den;
+    u(i) = (a(i)*u(i-2) + bet*u(i-1) - f(i)) * den;
+  }
 
   // backsubstitution
-  u(n-1) = u(n-1) + p(n-1) * u(n)
-  do i=n-2,1,-1
-   u(i) = u(i) + p(i) * u(i+1) + q(i) * u(i+2)
-  enddo
+  u(n-2) = u(n-2) + p(n-2) * u(n-1);
+  for (int i=n-3; i >= 0; i--) {
+    u(i) = u(i) + p(i) * u(i+1) + q(i) * u(i+2);
+  }
 }
 
 
