5978b82ca91f03f3c1512889b485ac81d0da176a
Isaac <irlyngaas@github.com>
2021-02-16 13:44:50 -0500

    Adding summit openMP build script

---
+++ unit/build/summit_openMP.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+source $MODULESHOME/init/bash
+module purge
+module load DefApps xl cuda cmake
+
+./cmakeclean.sh
+
+unset GATOR_DISABLE
+
+export CC=mpicc
+export CXX=mpic++
+export CXXFLAGS="-O3 -qsmp=omp -qoffload"
+export FFLAGS="-O3"
+
+
+cmake -DARCH="OPENMP45" \
+      -DOPENMP45_FLAGS="-03" \
+      ..
+
