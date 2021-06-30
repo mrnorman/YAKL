2059102d7b61eb07e4b622e048ab132c1e687c15
Matt Norman <normanmr@ornl.gov>
2021-06-30 09:49:51 -0400

    Getting rid of setting CXXFLAGS in all env files

--- unit/build/fhqwhgads_debug.sh
+++ unit/build/fhqwhgads_debug.sh
@@ -6,8 +6,8 @@ unset GATOR_DISABLE
 
 export CC=gcc
 export CXX=g++
-export CXXFLAGS="-O0 -g -DYAKL_DEBUG"
+unset CXXFLAGS
 export FFLAGS="-O0 -g"
 
-cmake -DYAKL_CXX_FLAGS="$CXXFLAGS" ..
+cmake -DYAKL_CXX_FLAGS="-O0 -g -DYAKL_DEBUG" ..
 
