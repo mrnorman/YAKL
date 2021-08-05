c4fde2067a562c7917a42b5da57e109551714b85
Matt Norman <normanmr@ornl.gov>
2021-08-05 10:56:30 -0400

    Fixing matinv routines to not overwrite the input array

--- YAKL_Array.h
+++ YAKL_Array.h
@@ -405,12 +405,14 @@ matmul_rc ( FSArray<T,2,SB<ROW_L>,SB<COL_L>> const &left ,
 // for column-row format
 /////////////////////////////////////////////////////////////////
 template <unsigned int n, class real>
-YAKL_INLINE SArray<real,2,n,n> matinv_ge_cr(SArray<real,2,n,n> &a) {
+YAKL_INLINE SArray<real,2,n,n> matinv_ge_cr(SArray<real,2,n,n> const &a) {
+  SArray<real,2,n,n> scratch;
   SArray<real,2,n,n> inv;
 
   // Initialize inverse as identity
   for (int icol = 0; icol < n; icol++) {
     for (int irow = 0; irow < n; irow++) {
+      scratch(icol,irow) = a(icol,irow);
       if (icol == irow) {
         inv(icol,irow) = 1;
       } else {
@@ -422,20 +424,20 @@ YAKL_INLINE SArray<real,2,n,n> matinv_ge_cr(SArray<real,2,n,n> &a) {
   // Gaussian elimination to zero out lower
   for (int idiag = 0; idiag < n; idiag++) {
     // Divide out the diagonal component from the first row
-    real factor = static_cast<real>(1)/a(idiag,idiag);
+    real factor = static_cast<real>(1)/scratch(idiag,idiag);
     for (int icol = idiag; icol < n; icol++) {
-      a(icol,idiag) *= factor;
+      scratch(icol,idiag) *= factor;
     }
     for (int icol = 0; icol < n; icol++) {
       inv(icol,idiag) *= factor;
     }
     for (int irow = idiag+1; irow < n; irow++) {
-      real factor = a(idiag,irow);
+      real factor = scratch(idiag,irow);
       for (int icol = idiag; icol < n; icol++) {
-        a  (icol,irow) -= factor * a  (icol,idiag);
+        scratch(icol,irow) -= factor * scratch(icol,idiag);
       }
       for (int icol = 0; icol < n; icol++) {
-        inv(icol,irow) -= factor * inv(icol,idiag);
+        inv    (icol,irow) -= factor * inv    (icol,idiag);
       }
     }
   }
@@ -443,12 +445,12 @@ YAKL_INLINE SArray<real,2,n,n> matinv_ge_cr(SArray<real,2,n,n> &a) {
   // Gaussian elimination to zero out upper
   for (int idiag = n-1; idiag >= 1; idiag--) {
     for (int irow = 0; irow < idiag; irow++) {
-      real factor = a(idiag,irow);
+      real factor = scratch(idiag,irow);
       for (int icol = irow+1; icol < n; icol++) {
-        a  (icol,irow) -= factor * a  (icol,idiag);
+        scratch(icol,irow) -= factor * scratch(icol,idiag);
       }
       for (int icol = 0; icol < n; icol++) {
-        inv(icol,irow) -= factor * inv(icol,idiag);
+        inv    (icol,irow) -= factor * inv    (icol,idiag);
       }
     }
   }
@@ -462,12 +464,14 @@ YAKL_INLINE SArray<real,2,n,n> matinv_ge_cr(SArray<real,2,n,n> &a) {
 // for row-column format
 /////////////////////////////////////////////////////////////////
 template <unsigned int n, class real>
-YAKL_INLINE SArray<real,2,n,n> matinv_ge_rc(SArray<real,2,n,n> &a) {
+YAKL_INLINE SArray<real,2,n,n> matinv_ge_rc(SArray<real,2,n,n> const &a) {
+  SArray<real,2,n,n> scratch;
   SArray<real,2,n,n> inv;
 
   // Initialize inverse as identity
   for (int icol = 0; icol < n; icol++) {
     for (int irow = 0; irow < n; irow++) {
+      scratch(icol,irow) = a(icol,irow);
       if (icol == irow) {
         inv(irow,icol) = 1;
       } else {
@@ -479,20 +483,20 @@ YAKL_INLINE SArray<real,2,n,n> matinv_ge_rc(SArray<real,2,n,n> &a) {
   // Gaussian elimination to zero out lower
   for (int idiag = 0; idiag < n; idiag++) {
     // Divide out the diagonal component from the first row
-    real factor = static_cast<real>(1)/a(idiag,idiag);
+    real factor = static_cast<real>(1)/scratch(idiag,idiag);
     for (int icol = idiag; icol < n; icol++) {
-      a(idiag,icol) *= factor;
+      scratch(idiag,icol) *= factor;
     }
     for (int icol = 0; icol < n; icol++) {
       inv(idiag,icol) *= factor;
     }
     for (int irow = idiag+1; irow < n; irow++) {
-      real factor = a(irow,idiag);
+      real factor = scratch(irow,idiag);
       for (int icol = idiag; icol < n; icol++) {
-        a  (irow,icol) -= factor * a  (idiag,icol);
+        scratch(irow,icol) -= factor * scratch(idiag,icol);
       }
       for (int icol = 0; icol < n; icol++) {
-        inv(irow,icol) -= factor * inv(idiag,icol);
+        inv    (irow,icol) -= factor * inv    (idiag,icol);
       }
     }
   }
@@ -500,12 +504,12 @@ YAKL_INLINE SArray<real,2,n,n> matinv_ge_rc(SArray<real,2,n,n> &a) {
   // Gaussian elimination to zero out upper
   for (int idiag = n-1; idiag >= 1; idiag--) {
     for (int irow = 0; irow < idiag; irow++) {
-      real factor = a(irow,idiag);
+      real factor = scratch(irow,idiag);
       for (int icol = irow+1; icol < n; icol++) {
-        a  (irow,icol) -= factor * a  (idiag,icol);
+        scratch(irow,icol) -= factor * scratch(idiag,icol);
       }
       for (int icol = 0; icol < n; icol++) {
-        inv(irow,icol) -= factor * inv(idiag,icol);
+        inv    (irow,icol) -= factor * inv    (idiag,icol);
       }
     }
   }
