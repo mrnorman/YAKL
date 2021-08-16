ff930b6f3219783951630941f5cca63ddb688a17
Matt Norman <normanmr@ornl.gov>
2021-08-16 11:15:07 -0400

    Adding new target-based YAKL cmake utility

--- yakl_utils.cmake
+++ yakl_utils.cmake
@@ -1,6 +1,20 @@
 
-macro(process_cxx_source_files files)
 
+function(get_lang_from_list files lang langfiles)
+  set(lang_files_loc "")
+  foreach(file ${files})
+    get_source_file_property(lang_loc ${file} LANGUAGE)
+    if ("${lang_loc}" STREQUAL "${lang}")
+      set(lang_files_loc "${lang_files_loc} ${file}")
+    endif()
+  endforeach()
+  string(REGEX REPLACE "^ " "" lang_files_loc "${lang_files_loc}")
+  set(${langfiles} "${lang_files_loc}" PARENT_SCOPE)
+endfunction()
+
+
+
+macro(process_cxx_source_files files)
   if ("${YAKL_ARCH}" STREQUAL "CUDA")
     set_source_files_properties(${files} PROPERTIES LANGUAGE CUDA)
     set(YAKL_COMPILER_FLAGS "-DYAKL_ARCH_CUDA --expt-extended-lambda --expt-relaxed-constexpr ${YAKL_CUDA_FLAGS}")
@@ -17,7 +31,24 @@ macro(process_cxx_source_files files)
   endif()
 
   set_source_files_properties(${files} PROPERTIES COMPILE_FLAGS "${YAKL_COMPILER_FLAGS}")
-
 endmacro(process_cxx_source_files)
+
+
+
+
+macro(process_target tname)
+  get_target_property(files ${tname} SOURCES)
+  get_lang_from_list("${files}" "CXX" cxxfiles)
+
+  process_cxx_source_files("${cxxfiles}")
+
+  if ("${YAKL_ARCH}" STREQUAL "CUDA")
+    set_property(TARGET ${tname} PROPERTY CUDA_STANDARD 14)
+  endif()
+  set_property(TARGET ${tname} PROPERTY CXX_STANDARD 14)
+
+  target_link_libraries(${tname} yakl)
+endmacro(process_target)
+
 
 
