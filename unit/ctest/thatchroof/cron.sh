54dbc927bcfd6982f76069ae576601a82ded45ca
Matt Norman <normanmr@ornl.gov>
2021-08-25 12:57:54 -0400

    Adding cron job for thatchroof

---
+++ unit/ctest/thatchroof/cron.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd /home/imn/yakl_ctest/scripts
+
+curl -O https://raw.githubusercontent.com/mrnorman/YAKL/master/unit/ctest/thatchroof/master-serial-debug.sh
+curl -O https://raw.githubusercontent.com/mrnorman/YAKL/master/unit/ctest/thatchroof/master-serial-debug-nogator.sh
+curl -O https://raw.githubusercontent.com/mrnorman/YAKL/master/unit/ctest/thatchroof/master-openmp-opt.sh
+curl -O https://raw.githubusercontent.com/mrnorman/YAKL/master/unit/ctest/thatchroof/ctest_script.cmake
+curl -O https://raw.githubusercontent.com/mrnorman/YAKL/master/unit/ctest/thatchroof/master-cuda-debug.sh
+curl -O https://raw.githubusercontent.com/mrnorman/YAKL/master/unit/ctest/thatchroof/run_all.sh
+curl -O https://raw.githubusercontent.com/mrnorman/YAKL/master/unit/ctest/thatchroof/master-cuda-opt.sh
+curl -O https://raw.githubusercontent.com/mrnorman/YAKL/master/unit/ctest/thatchroof/master-serial-opt.sh
+
+chmod 744 master*.sh
+
+./run_all.sh
+
