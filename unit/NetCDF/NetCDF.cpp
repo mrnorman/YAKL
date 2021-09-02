82187878c430c4b72b9d2e5ad5135634885266b4
Matt Norman <normanmr@ornl.gov>
2021-09-01 20:24:43 -0400

    New C-based NetCDF interface is passing tests

--- unit/NetCDF/NetCDF.cpp
+++ unit/NetCDF/NetCDF.cpp
@@ -46,16 +46,16 @@ int main() {
       nc.create( file_name ); // Default mode is overwrite when creating
 
       // Create the varaibles
-      Array<          char,1,memDevice,styleC      > a0("a0",d1);
-      Array<unsigned  char,2,memDevice,styleFortran> a1("a1",d1,d2);   // TODO: Fortran write1 seems to fail
-      Array<         short,3,memDevice,styleC      > a2("a2",d3,d2,d1);
-      Array<unsigned short,4,memDevice,styleFortran> a3("a3",d1,d2,d3,d4);
-      Array<           int,5,memDevice,styleC      > a4("a4",d5,d4,d3,d2,d1);
-      Array<unsigned   int,6,memHost  ,styleFortran> a5("a5",d1,d2,d3,d4,d5,d6);
-      Array<          long,7,memHost  ,styleC      > a6("a6",d7,d6,d5,d4,d3,d2,d1);
-      Array<unsigned  long,8,memHost  ,styleFortran> a7("a7",d1,d2,d3,d4,d5,d6,d7,d8);
-      Array<         float,3,memHost  ,styleC      > a8("a8",d3,d2,d1);
-      Array<        double,3,memHost  ,styleFortran> a9("a9",d1,d2,d3);
+      Array<signed        char,1,memDevice,styleC      > a0("a0",d1);
+      Array<unsigned      char,2,memDevice,styleFortran> a1("a1",d1,d2);
+      Array<             short,3,memDevice,styleC      > a2("a2",d3,d2,d1);
+      Array<unsigned     short,4,memDevice,styleFortran> a3("a3",d1,d2,d3,d4);
+      Array<               int,5,memDevice,styleC      > a4("a4",d5,d4,d3,d2,d1);
+      Array<unsigned       int,6,memHost  ,styleFortran> a5("a5",d1,d2,d3,d4,d5,d6);
+      Array<         long long,7,memHost  ,styleC      > a6("a6",d7,d6,d5,d4,d3,d2,d1);
+      Array<unsigned long long,8,memHost  ,styleFortran> a7("a7",d1,d2,d3,d4,d5,d6,d7,d8);
+      Array<             float,3,memHost  ,styleC      > a8("a8",d3,d2,d1);
+      Array<            double,3,memHost  ,styleFortran> a9("a9",d1,d2,d3);
       float s0 = 1;
       int   s1 = 2;
 
@@ -82,10 +82,8 @@ int main() {
       nc.write( a7 , "a7" , {"d1","d2","d3","d4","d5","d6","d7","d8"} );
       nc.write( a8 , "a8" , {"d3","d2","d1"} );
       nc.write( a9 , "a9" , {"d1","d2","d3"} );
-
-      // Write scalars to file
-      nc.write( s0 , "s0" );
-      nc.write( s1 , "s1" );
+      nc.write( s0  , "s0" );
+      nc.write( s1  , "s1" );
     
       // Create a dimension that isn't used
       nc.createDim( "nobody_likes_me" , 100 );
@@ -147,30 +145,29 @@ int main() {
 
       // We're going to permute the memory space and Array style to ensure it's written and read correctly
       // If the dimensions are off, there will be an error thrown from YAKL_netcdf
-      Array<          char,1,memDevice,styleC      > a0("a0",d1);
-      Array<unsigned  char,2,memHost  ,styleC      > a1("a1",d2,d1);
-      Array<         short,3,memDevice,styleC      > a2("a2",d3,d2,d1);
-      Array<unsigned short,4,memHost  ,styleC      > a3("a3",d4,d3,d2,d1);
-      Array<           int,5,memDevice,styleC      > a4("a4",d5,d4,d3,d2,d1);
-      Array<unsigned   int,6,memHost  ,styleFortran> a5("a5",d1,d2,d3,d4,d5,d6);
-      Array<          long,7,memDevice,styleFortran> a6("a6",d1,d2,d3,d4,d5,d6,d7);
-      Array<unsigned  long,8,memHost  ,styleFortran> a7("a7",d1,d2,d3,d4,d5,d6,d7,d8);
-      Array<         float,3,memDevice,styleFortran> a8("a8",d1,d2,d3);
-      Array<        double,3,memHost  ,styleFortran> a9("a9",d1,d2,d3);
-      float s0 = -999;
-      int   s1 = -999;
-      Array<          char,2,memDevice,styleC      > a0_unlim("a0_unlim",num_entries,d1);
-      Array<unsigned  char,3,memHost  ,styleC      > a1_unlim("a1_unlim",num_entries,d2,d1);
-      Array<         short,4,memDevice,styleC      > a2_unlim("a2_unlim",num_entries,d3,d2,d1);
-      Array<unsigned short,5,memHost  ,styleC      > a3_unlim("a3_unlim",num_entries,d4,d3,d2,d1);
-      Array<           int,6,memDevice,styleC      > a4_unlim("a4_unlim",num_entries,d5,d4,d3,d2,d1);
-      Array<unsigned   int,7,memHost  ,styleFortran> a5_unlim("a5_unlim",d1,d2,d3,d4,d5,d6,num_entries);
-      Array<          long,8,memDevice,styleFortran> a6_unlim("a6_unlim",d1,d2,d3,d4,d5,d6,d7,num_entries);
-      // Array<unsigned  long,9,memHost  ,styleFortran> a7_unlim("a7_unlim",d1,d2,d3,d4,d5,d6,d7,d8,num_entries);
-      Array<         float,4,memDevice,styleFortran> a8_unlim("a8_unlim",d1,d2,d3,num_entries);
-      Array<        double,4,memHost  ,styleFortran> a9_unlim("a9_unlim",d1,d2,d3,num_entries);
+      Array<signed        char,1,memDevice,styleC      > a0("a0",d1);
+      Array<unsigned      char,2,memHost  ,styleC      > a1("a1",d2,d1);
+      Array<             short,3,memDevice,styleC      > a2("a2",d3,d2,d1);
+      Array<unsigned     short,4,memHost  ,styleC      > a3("a3",d4,d3,d2,d1);
+      Array<               int,5,memDevice,styleC      > a4("a4",d5,d4,d3,d2,d1);
+      Array<unsigned       int,6,memHost  ,styleFortran> a5("a5",d1,d2,d3,d4,d5,d6);
+      Array<         long long,7,memDevice,styleFortran> a6("a6",d1,d2,d3,d4,d5,d6,d7);
+      Array<unsigned long long,8,memHost  ,styleFortran> a7("a7",d1,d2,d3,d4,d5,d6,d7,d8);
+      Array<             float,3,memDevice,styleFortran> a8("a8",d1,d2,d3);
+      Array<            double,3,memHost  ,styleFortran> a9("a9",d1,d2,d3);
+      Array<signed        char,2,memDevice,styleC      > a0_unlim("a0_unlim",num_entries,d1);
+      Array<unsigned      char,3,memHost  ,styleC      > a1_unlim("a1_unlim",num_entries,d2,d1);
+      Array<             short,4,memDevice,styleC      > a2_unlim("a2_unlim",num_entries,d3,d2,d1);
+      Array<unsigned     short,5,memHost  ,styleC      > a3_unlim("a3_unlim",num_entries,d4,d3,d2,d1);
+      Array<               int,6,memDevice,styleC      > a4_unlim("a4_unlim",num_entries,d5,d4,d3,d2,d1);
+      Array<unsigned       int,7,memHost  ,styleFortran> a5_unlim("a5_unlim",d1,d2,d3,d4,d5,d6,num_entries);
+      Array<         long long,8,memDevice,styleFortran> a6_unlim("a6_unlim",d1,d2,d3,d4,d5,d6,d7,num_entries);
+      Array<             float,4,memDevice,styleFortran> a8_unlim("a8_unlim",d1,d2,d3,num_entries);
+      Array<            double,4,memHost  ,styleFortran> a9_unlim("a9_unlim",d1,d2,d3,num_entries);
       Array<float,1,memHost  ,styleC> s0_unlim("s0_unlim",num_entries);
       Array<int  ,1,memDevice,styleC> s1_unlim("s1_unlim",num_entries);
+      float s0;
+      int   s1;
 
       nc.read( a0 , "a0" );
       nc.read( a1 , "a1" );
@@ -191,7 +188,6 @@ int main() {
       nc.read( a4_unlim , "a4_unlim" );
       nc.read( a5_unlim , "a5_unlim" );
       nc.read( a6_unlim , "a6_unlim" );
-      // nc.read( a7_unlim , "a7_unlim" );
       nc.read( a8_unlim , "a8_unlim" );
       nc.read( a9_unlim , "a9_unlim" );
       nc.read( s0_unlim , "s0_unlim" );
@@ -212,8 +208,6 @@ int main() {
       if ( sum(a7) / size(a7) != 7 ) die("ERROR: avg of a7 should be 7");
       if ( sum(a8) / size(a8) != 8 ) die("ERROR: avg of a8 should be 8");
       if ( sum(a9) / size(a9) != 9 ) die("ERROR: avg of a9 should be 9");
-      if ( s0 != 1 ) die("ERROR: s0 should be 1");
-      if ( s1 != 2 ) die("ERROR: s1 should be 2");
 
       if ( sum(a0_unlim) / size(a0_unlim) != 0 ) die("ERROR: avg of a0_unlim should be 0");
       if ( sum(a1_unlim) / size(a1_unlim) != 1 ) die("ERROR: avg of a1_unlim should be 1");
@@ -222,7 +216,6 @@ int main() {
       if ( sum(a4_unlim) / size(a4_unlim) != 4 ) die("ERROR: avg of a4_unlim should be 4");
       if ( sum(a5_unlim) / size(a5_unlim) != 5 ) die("ERROR: avg of a5_unlim should be 5");
       if ( sum(a6_unlim) / size(a6_unlim) != 6 ) die("ERROR: avg of a6_unlim should be 6");
-      // if ( sum(a7_unlim) / size(a7_unlim) != 7 ) die("ERROR: avg of a7_unlim should be 7");
       if ( sum(a8_unlim) / size(a8_unlim) != 8 ) die("ERROR: avg of a8_unlim should be 8");
       if ( sum(a9_unlim) / size(a9_unlim) != 9 ) die("ERROR: avg of a9_unlim should be 9");
       if ( sum(s0_unlim) / size(s0_unlim) != 1 ) die("ERROR: avg of s0_unlim should be 1");
