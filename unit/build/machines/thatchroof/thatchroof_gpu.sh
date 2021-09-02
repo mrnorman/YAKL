c3dfa10e11622dc2bc3fe0b1e08b5433312032db
Matt Norman <normanmr@ornl.gov>
2021-09-01 20:42:30 -0400

    Changing thatchroof unit files. need to change CTest scripts to switch to normal netcdf on all machines tomorrow

--- unit/build/machines/thatchroof/thatchroof_gpu.sh
+++ unit/build/machines/thatchroof/thatchroof_gpu.sh
@@ -11,8 +11,9 @@ unset CXXFLAGS
 unset FFLAGS
 
 cmake -DYAKL_ARCH="CUDA"                                                                               \
-      -DYAKL_CUDA_FLAGS="-O3 -arch sm_35 --use_fast_math -ccbin g++ -DTHRUST_IGNORE_CUB_VERSION_CHECK" \
+      -DYAKL_CUDA_FLAGS="-O3 -arch sm_35 --use_fast_math -ccbin g++ -DTHRUST_IGNORE_CUB_VERSION_CHECK -I/opt/netcdf_gnu/include" \
       -DYAKL_C_FLAGS="-O3"                                                                             \
       -DYAKL_F90_FLAGS="-O3"                                                                           \
+      -DNETCDF_LINK_FLAGS="`/opt/netcdf_gnu/bin/nc-config --libs`"        \
       ../../..
 
