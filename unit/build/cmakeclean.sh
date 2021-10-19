e0abf8b310356595960dfbdc52fa5a80d02fd6e9
Matt Norman <normanmr@ornl.gov>
2021-10-19 13:40:44 -0400

    Improving cleanup for the cmakeclean script

--- unit/build/cmakeclean.sh
+++ unit/build/cmakeclean.sh
@@ -3,5 +3,5 @@
 rm -rf CArray  CMakeCache.txt  CMakeFiles  cmake_install.cmake  CTestTestfile.cmake  Makefile  \
        Testing  yakl  FArray  Gator  Random  FFT  Reductions  Atomics  CPackConfig.cmake       \
        CPackSourceConfig.cmake  Pentadiagonal  Tridiagonal  Lambda  Fortran_Link               \
-       OpenMP_Regions
+       OpenMP_Regions  Fortran_Gator  Intrinsics  NetCDF  ParForC  ParForFortran
 
