9b5d8831ee6ec9e5021fde3de7ef865c3a9522f5
Matt Norman <normanmr@ornl.gov>
2022-03-04 17:46:37 -0500

    Committing crusher unit test machine file

---
+++ unit/build/machines/crusher/crusher_amd_hip.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+source $MODULESHOME/init/bash
+module load PrgEnv-amd craype-accel-amd-gfx90a rocm
+
+../../cmakeclean.sh
+
+unset GATOR_DISABLE
+
+export CC=cc
+export CXX=CC
+export FC=ftn
+unset CXXFLAGS
+unset FFLAGS
+
+cmake -DYAKL_ARCH="HIP"                                \
+      -DYAKL_HIP_FLAGS="-O3 -std=c++11 -D__HIP_ROCclr__ -D__HIP_ARCH_GFX90A__=1 --rocm-path=${ROCM_PATH} --offload-arch=gfx90a -x hip" \
+      -DYAKL_F90_FLAGS="-O3"                            \
+      -DCMAKE_EXE_LINKER_FLAGS="--rocm-path=${ROCM_PATH} -L${ROCM_PATH}/lib -lamdhip64" \
+      -DMPI_COMMAND="" \
+      ../../..
+
