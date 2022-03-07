4ca1071745c079e9deefb7917c8031629ff09e53
Matt Norman <normanmr@ornl.gov>
2022-03-07 18:02:17 -0500

    Fixing HIP inline issue in YAKL_defines

--- src/YAKL_defines.h
+++ src/YAKL_defines.h
@@ -17,7 +17,7 @@
 
   #define YAKL_LAMBDA [=] __host__ __device__
   #define YAKL_DEVICE_LAMBDA [=] __device__
-  #define YAKL_INLINE inline __host__ __device__ __forceinline__
+  #define YAKL_INLINE __host__ __device__ __forceinline__
   #define YAKL_DEVICE_INLINE inline __device__
   #define YAKL_SCOPE(a,b) auto &a = std::ref(b).get()
   #define YAKL_SEPARATE_MEMORY_SPACE
