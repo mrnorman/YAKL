d023f6c4cedee95611a8ccd767129db0128eef12
Matt Norman <normanmr@ornl.gov>
2022-04-27 18:09:38 -0400

    Improving efficiency of count()

--- src/YAKL_Intrinsics.h
+++ src/YAKL_Intrinsics.h
@@ -1097,11 +1097,9 @@ namespace yakl {
       #ifdef YAKL_DEBUG
         if (!mask.initialized()) { yakl_throw("ERROR: calling count on an array that has not been initialized"); }
       #endif
-      ScalarLiveOut<int> numTrue(0);
-      c::parallel_for( c::SimpleBounds<1>( mask.totElems() ) , YAKL_LAMBDA (int i) {
-        if (mask.data()[i]) { atomicAdd(numTrue(),1); }
-      });
-      return numTrue.hostRead();
+      auto intarr = mask.template createDeviceObject<int>();
+      c::parallel_for( mask.totElems() , YAKL_LAMBDA (int i) { intarr.data()[i] = mask.data()[i] ? 1 : 0; });
+      return yakl::intrinsics::sum(intarr);
     }
     template <int rank, class D0, class D1, class D2, class D3>
     YAKL_INLINE int count( FSArray<bool,rank,D0,D1,D2,D3> const &mask ) {
