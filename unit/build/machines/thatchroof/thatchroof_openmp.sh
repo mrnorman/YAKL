365bef8429f04de23c675d9732dfd7aadc38530b
Matt Norman <normanmr@ornl.gov>
2022-07-26 17:04:47 -0400

    Fixes Issue #68 : fixes parallel_inner overriding threading from parallel_outer in OpenMP 3.5 backend

--- unit/build/machines/thatchroof/thatchroof_openmp.sh
+++ unit/build/machines/thatchroof/thatchroof_openmp.sh
@@ -15,9 +15,8 @@ unset CXXFLAGS
 unset FFLAGS
 
 cmake -DYAKL_ARCH="OPENMP"               \
-      -DYAKL_OPENMP_FLAGS="-O3 -fopenmp -I/opt/netcdf_gnu/include" \
-      -DYAKL_C_FLAGS="-O3"               \
+      -DYAKL_OPENMP_FLAGS="-O3 -fopenmp -I`nc-config --includedir`" \
       -DYAKL_F90_FLAGS="-O3"             \
-      -DNETCDF_LINK_FLAGS="`/opt/netcdf_gnu/bin/nc-config --libs`"        \
+      -DNETCDF_LINK_FLAGS="`nc-config --libs`"        \
       ../../..
 
