153ead69eb194febccca1a73ccbbda12bf7ca750
Matt Norman <normanmr@ornl.gov>
2022-08-09 14:13:20 -0400

    Updating cmakeclean to clean up Streams unit test folder

--- unit/build/cmakeclean.sh
+++ unit/build/cmakeclean.sh
@@ -3,5 +3,6 @@
 rm -rf CArray  CMakeCache.txt  CMakeFiles  cmake_install.cmake  CTestTestfile.cmake  Makefile  \
        Testing  yakl  FArray  Gator  Random  FFT  Reductions  Atomics  CPackConfig.cmake       \
        CPackSourceConfig.cmake  Pentadiagonal  Tridiagonal  Lambda  Fortran_Link               \
-       OpenMP_Regions  Fortran_Gator  Intrinsics  NetCDF  ParForC  ParForFortran  performance
+       OpenMP_Regions  Fortran_Gator  Intrinsics  NetCDF  ParForC  ParForFortran  performance  \
+       Streams 
 
