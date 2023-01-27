5e516d6d93e66309630b437d35aa28e291d4bec6
Matt Norman <normanmr@ornl.gov>
2023-01-27 08:00:44 -0500

    Revert "[SYCL] updated AoT for PVC devices"
    This reverts commit e766e56c6aecb6869b46f338861b3f7f6808eb60.

--- unit/build/machines/jlse/jlse_gpu_O3_AoT_PVC.sh
+++ unit/build/machines/jlse/jlse_gpu_O3_AoT_PVC.sh
@@ -16,7 +16,7 @@ unset FFLAGS
 
 cmake -DYAKL_ARCH="SYCL" \
       -DYAKL_SYCL_FLAGS="-O3" \
-      -DCMAKE_CXX_FLAGS="-O3 -fsycl -sycl-std=2020 -fsycl-unnamed-lambda -fsycl-default-sub-group-size=16 -fsycl-device-code-split=per_kernel -fsycl-targets=spir64_gen -Xsycl-target-backend \"-device 12.60.7\"" \
+      -DCMAKE_CXX_FLAGS="-O3 -fsycl -sycl-std=2020 -fsycl-unnamed-lambda -fsycl-default-sub-group-size=16 -fsycl-device-code-split=per_kernel -fsycl-targets=spir64_gen -Xsycl-target-backend \"-device 12.4.0\"" \
       -DYAKL_F90_FLAGS="-O3" \
       -DYAKL_C_FLAGS="-O3"   \
       ../../..
