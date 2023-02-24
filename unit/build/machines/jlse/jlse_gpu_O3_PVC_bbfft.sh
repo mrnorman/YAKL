f94766bf5c3b1f8fadacb89202346d503264dbd6
Omar Khalil Ahmed <omar.ahmed@intel.com>
2023-02-23 16:49:06 -0800

    Add jlse unit test build scripts

---
+++ unit/build/machines/jlse/jlse_gpu_O3_PVC_bbfft.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+module purge
+module use /soft/restricted/CNDA/modulefiles
+module load oneapi/eng-compiler/2022.12.30.003
+module use /soft/modulefiles
+module load cmake/3.23.2
+
+export PATH=/home/ac.oahmed2/cmake/cmake-3.26.0-rc4-linux-x86_64/bin:$PATH
+
+unset GATOR_DISABLE
+
+export CC=icx
+export CXX=icpx
+export FC=ifx
+unset CXXFLAGS
+unset FFLAGS
+
+../../cmakeclean.sh
+
+cmake -DYAKL_ARCH="SYCL" \
+      -DYAKL_SYCL_FLAGS="-fsycl -sycl-std=2020 -fsycl-unnamed-lambda -fsycl-default-sub-group-size=16 -fsycl-device-code-split=per_kernel " \
+      -DCMAKE_CXX_FLAGS="-O3" \
+      -DYAKL_F90_FLAGS="-O3" \
+      -DYAKL_C_FLAGS="-O3"   \
+      -DYAKL_SYCL_BBFFT=1      \                                                                                                     \
+      -DYAKL_SYCL_BBFFT_AOT=0       \                                                                                            \
+      ../../..
+
+
+make -j32 FFT
