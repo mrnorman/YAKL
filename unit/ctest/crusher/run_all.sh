4ca04fca7b21e52d9ab81143d0281aa20f907259
Sarat Sreepathi <sarat@ornl.gov>
2023-03-01 00:35:11 -0500

    Crusher CTest add valgrind options

--- unit/ctest/crusher/run_all.sh
+++ unit/ctest/crusher/run_all.sh
@@ -4,4 +4,6 @@
 ./main-crayclang-opt.sh  
 ./main-hipcc-debug.sh  
 ./main-hipcc-opt.sh
-
+# Comment out below if Valgrind is not needed
+# ./main-hipcc-opt-valgrind.sh
+./main-hipcc-debug-valgrind.sh
