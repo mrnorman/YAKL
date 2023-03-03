fe55d5af87f0be31d7d7ef3bdb81a1c5257a7b92
Sarat Sreepathi <sarat@login05.chn>
2023-03-02 19:50:48 -0800

    Add system modules

--- unit/ctest/perlmutter/main-nvidia-opt.sh
+++ unit/ctest/perlmutter/main-nvidia-opt.sh
@@ -5,18 +5,20 @@ module reset
 module load cmake PrgEnv-nvidia cudatoolkit craype-accel-nvidia80
 
 test_home=${PSCRATCH}/yakl-testing
-rm -rf ${test_home}
-mkdir ${test_home}
+mkdir -p ${test_home}
+rm -rf ${test_home}/YAKL
+rm -rf ${test_home}/scratch
+
 export CTEST_BUILD_NAME=main-nvidia-opt
 
 export CC=cc
 export CXX=CC
 export FC=ftn
 
-export YAKL_CTEST_SRC=${test_home}/../YAKL
+export YAKL_CTEST_SRC=${test_home}/YAKL
 export YAKL_CTEST_BIN=${test_home}/scratch
 export CTEST_YAKL_ARCH="CUDA"
-export CTEST_CUDA_FLAGS="-O3 --use_fast_math -arch sm_70 -ccbin g++ -DTHRUST_IGNORE_CUB_VERSION_CHECK"
+export CTEST_CUDA_FLAGS="-O3 --use_fast_math -arch sm_80 -ccbin CC -DTHRUST_IGNORE_CUB_VERSION_CHECK"
 export CTEST_C_FLAGS="-O3"
 export CTEST_F90_FLAGS="-O3"
 export CTEST_LD_FLAGS=""
@@ -26,11 +28,9 @@ export CTEST_MPI_COMMAND=""
 
 ctest_dir=`pwd`
 cd ${YAKL_CTEST_SRC}
-git fetch origin
-git checkout main
-git reset --hard origin/main
+git clone --recurse-submodules git@github.com:E3SM-Project/YAKL.git
+git checkout sarats/perlmutter
 git submodule update --init --recursive
-
 
 cd ${ctest_dir}
 
