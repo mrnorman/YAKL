b239ed3c8ff8c703d0962526e105a40fccfbc3e5
Sarat Sreepathi <sarat@login05.chn>
2023-03-02 20:43:54 -0800

    Ctest setup for Perlmutter verified.

--- unit/ctest/perlmutter/main-nvidia-debug.sh
+++ unit/ctest/perlmutter/main-nvidia-debug.sh
@@ -10,7 +10,7 @@ mkdir -p ${test_home}
 rm -rf ${test_home}/YAKL
 rm -rf ${test_home}/scratch
 
-export CTEST_BUILD_NAME=main-nvidia-opt
+export CTEST_BUILD_NAME=main-nvidia-debug
 
 export CC=cc
 export CXX=CC
@@ -19,9 +19,9 @@ export FC=ftn
 export YAKL_CTEST_SRC=${test_home}/YAKL
 export YAKL_CTEST_BIN=${test_home}/scratch
 export CTEST_YAKL_ARCH="CUDA"
-export CTEST_CUDA_FLAGS="-O3 --use_fast_math -arch sm_80 -ccbin CC -DTHRUST_IGNORE_CUB_VERSION_CHECK"
-export CTEST_C_FLAGS="-O3"
-export CTEST_F90_FLAGS="-O3"
+export CTEST_CUDA_FLAGS="-O0 -DYAKL_DEBUG -arch sm_80 -ccbin CC -DTHRUST_IGNORE_CUB_VERSION_CHECK"
+export CTEST_C_FLAGS="-O0 -g -DYAKL_DEBUG"
+export CTEST_F90_FLAGS="-O0 -g -DYAKL_DEBUG"
 export CTEST_LD_FLAGS=""
 export CTEST_GCOV=0
 export CTEST_VALGRIND=0
@@ -29,7 +29,7 @@ export CTEST_MPI_COMMAND=""
 
 ctest_dir=`pwd`
 cd ${test_home}
-git clone --recurse-submodules git@github.com:E3SM-Project/YAKL.git
+git clone --recurse-submodules git@github.com:mrnorman/YAKL.git
 
 cd ${ctest_dir}
 
