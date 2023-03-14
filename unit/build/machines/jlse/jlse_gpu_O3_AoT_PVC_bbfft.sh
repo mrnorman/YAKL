8774acd0827b11b276c9a6f135e35703cb067067
Omar Khalil Ahmed <omar.ahmed@intel.com>
2023-03-14 13:32:12 -0700

    Add bbfft build configurations for JLSE

---
+++ unit/build/machines/jlse/jlse_gpu_O3_AoT_PVC_bbfft.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+module purge
+module use /soft/restricted/CNDA/modulefiles
+module load oneapi
+module use /soft/modulefiles
+module load cmake/3.23.2
+module load bbfft
+
+../../cmakeclean.sh
+
+unset GATOR_DISABLE
+
+export CC=mpiicx
+export CXX=mpiicpx
+export FC=mpiifx
+unset CXXFLAGS
+unset FFLAGS
+
+cmake -DYAKL_ARCH="SYCL" \
+      -DYAKL_SYCL_FLAGS="-fsycl -sycl-std=2020 -fsycl-unnamed-lambda -fsycl-default-sub-group-size=16 -fsycl-device-code-split=per_kernel -fsycl-targets=spir64_gen -Xsycl-target-backend \"-device 0x0bd5 -revision_id 0x2f\"" \
+      -DCMAKE_CXX_FLAGS="-O3" \
+      -DYAKL_F90_FLAGS="-O3" \
+      -DYAKL_C_FLAGS="-O3"   \
+      -DYAKL_SYCL_BBFFT=1      \
+      -DYAKL_SYCL_BBFFT_AOT=1    \
+      -DYAKL_SYCL_BBFFT_AOT_LEGACY_UMD=1 \
+      ../../..
+
+
+make -j32 FFT
