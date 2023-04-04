18aefdb364b6f061c888057ee81d11f4fd66961b
Omar Khalil Ahmed <omar.ahmed@intel.com>
2023-04-04 10:39:59 -0700

    Updated source files for bbfft

---
+++ external/bbfft/aot_compiled_kernels.hpp
@@ -0,0 +1,9 @@
+#ifndef AOT_COMPILED_KERNELS_20230202_HPP
+#define AOT_COMPILED_KERNELS_20230202_HPP
+
+#include <string>
+#include <unordered_set>
+
+extern const std::unordered_set<std::string> aot_compiled_kernels;
+
+#endif // AOT_COMPILED_KERNELS_20230202_HPP
