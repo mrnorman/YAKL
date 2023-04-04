eff23c61137d589f331013a282b94ad1e2d1aed2
Omar Khalil Ahmed <omar.ahmed@intel.com>
2023-04-04 10:44:40 -0700

    Update FFT unit test for bbfft

---
+++ unit/FFT/bbfft/configurations.hpp
@@ -0,0 +1,13 @@
+// Copyright (C) 2023 Intel Corporation
+// SPDX-License-Identifier: BSD-3-Clause
+
+#ifndef CONFIGURATIONS_20230202_HPP
+#define CONFIGURATIONS_20230202_HPP
+
+#include "bbfft/configuration.hpp"
+
+#include <vector>
+
+std::vector<bbfft::configuration> configurations();
+
+#endif // CONFIGURATIONS_20230202_HPP
