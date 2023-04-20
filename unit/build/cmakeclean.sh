1d943b8c231cc4798922aa1ffc6602ee8c59afca
Matt Norman <normanmr@ornl.gov>
2023-04-20 12:48:33 -0400

    Fixes transpose bug on earlier Nvidia compilers

--- unit/build/cmakeclean.sh
+++ unit/build/cmakeclean.sh
@@ -4,5 +4,6 @@ rm -rf CArray  CMakeCache.txt  CMakeFiles  cmake_install.cmake  CTestTestfile.cm
        Testing  yakl  FArray  Gator  Random  FFT  Reductions  Atomics  CPackConfig.cmake       \
        CPackSourceConfig.cmake  Pentadiagonal  Tridiagonal  Lambda  Fortran_Link               \
        OpenMP_Regions  Fortran_Gator  Intrinsics  NetCDF  ParForC  ParForFortran  performance  \
-       Streams  include  install_manifest.txt  lib  yakl-variables.cmake  yakl-variables.make  
+       Streams  include  install_manifest.txt  lib  yakl-variables.cmake  yakl-variables.make  \
+       yakl_verbose_output_task_0.log
 
