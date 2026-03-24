74a20aea415af6a81b80c2048ea38a821d6b3cec
Matt Norman <normanmr@ornl.gov>
2026-03-24 10:28:49 -0400

    Working on intrinsics and creating mirror View functions

--- unit/Kokkos_PoolSpace/Kokkos_PoolSpace.cpp
+++ unit/Kokkos_PoolSpace/Kokkos_PoolSpace.cpp
@@ -5,12 +5,66 @@ void die(std::string msg) {
   Kokkos::abort(msg.c_str());
 }
 
+template <class ViewType>
+inline typename ViewType::non_const_value_type sum(ViewType const & a) {
+  using scalar_t = typename ViewType::non_const_value_type;
+  scalar_t result;
+  if constexpr (Kokkos::SpaceAccessibility<Kokkos::HostSpace,typename ViewType::memory_space>::accessible) {
+    result = a.data()[0];
+    for (int i=1; i < a.size(); i++) { result += a.data()[i]; }
+  } else {
+    Kokkos::parallel_reduce( "parallel_reduce_sum" , a.size() , KOKKOS_LAMBDA (int i , scalar_t & lsum ) {
+      lsum += a.data()[i];
+    }, result);
+  }
+  return result;
+}
+
+
+template <class ViewType>
+inline decltype(Kokkos::create_mirror(Kokkos::DefaultExecutionSpace::memory_space{},ViewType()))
+create_device_object(ViewType const &in) {
+  return Kokkos::create_mirror(Kokkos::DefaultExecutionSpace::memory_space{},in);
+}
+
+
+template <class ViewType>
+inline decltype(Kokkos::create_mirror(Kokkos::HostSpace{},ViewType()))
+create_host_object(ViewType const &in) {
+  return Kokkos::create_mirror(Kokkos::HostSpace{},in);
+}
+
+
+template <class ViewType>
+inline decltype(Kokkos::create_mirror(Kokkos::DefaultExecutionSpace::memory_space{},ViewType()))
+create_device_copy(ViewType const &in) {
+  auto out = Kokkos::create_mirror(Kokkos::DefaultExecutionSpace::memory_space{},in);
+  Kokkos::deep_copy(out,in);
+  return out;
+}
+
+
+template <class ViewType>
+inline decltype(Kokkos::create_mirror(Kokkos::HostSpace{},ViewType()))
+create_host_copy(ViewType const &in) {
+  auto out = Kokkos::create_mirror(Kokkos::HostSpace{},in);
+  Kokkos::deep_copy(out,in);
+  return out;
+}
+
 
 int main() {
   Kokkos::initialize();
   yakl::init();
   {
     Kokkos::View<float *,Kokkos::LayoutRight,yakl::PoolSpace> arr("arr",10);
+    Kokkos::deep_copy(arr,1);
+    auto arr_h = create_host_object(arr);
+    Kokkos::deep_copy(arr_h,arr);
+    auto arr_d = create_device_copy(arr_h);
+    std::cout << sum(arr  ) << std::endl;
+    std::cout << sum(arr_h) << std::endl;
+    std::cout << sum(arr_d) << std::endl;
   }
   yakl::finalize();
   Kokkos::finalize(); 
